3571587dec8367397d470fd5394eff9f1fcefd3e
Kaylin Norman-Slack <k.a.normanslack@gmail.com>
2026-09-05 18:10:27 -0700

    Restored StormCore to Initial format attempt #1

--- game_init.gd
+++ game_init.gd
@@ -3,13 +3,9 @@ extends Node
 
 func _ready():
 	# 1. Register core buses
-	GlobalBusManager.register_bus("UIBus", BaseEventBus.new())
-	GlobalBusManager.register_bus("GameBus", BaseEventBus.new())
-	GlobalBusManager.register_bus("AudioBus", BaseEventBus.new())
-	GlobalBusManager.register_bus("InputBus", BaseEventBus.new())
-	GlobalBusManager.register_bus("CameraBus", BaseEventBus.new())
-	GlobalBusManager.register_bus("SceneBus", BaseEventBus.new())
-
+	GlobalBusManager.register_bus("ui", BaseEventBus.new())
+	GlobalBusManager.register_bus("game", BaseEventBus.new())
+	GlobalBusManager.register_bus("audio", BaseEventBus.new())
 	
 	# 2. Load game adapters (auto-discovered)
 	#GlobalAdapterRegistry.adapter_folder = "res://game/adapters/"
@@ -23,18 +19,3 @@ func _ready():
 		var bus_name = adapter.preferred_bus_name
 		if bus_name:
 			adapter.listen_to_bus_named(bus_name)
-	_request_opening_scene()
-
-func _request_opening_scene() -> void:
-	var scene_bus := GlobalBusManager.get_bus("SceneBus")
-
-	if not scene_bus is BaseEventBus:
-		push_error("StormRoot could not resolve SceneBus.")
-		return
-
-	scene_bus.emit_event({
-		"event": "load_scene",
-		"payload": {
-			"scene_path": "res://scenes/opening/opening.tscn"
-		}
-	})
